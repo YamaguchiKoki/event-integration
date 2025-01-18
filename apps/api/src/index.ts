0c189f5ffd96a81e8831aea4b9824efc98bf3ca3
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-01-18 23:52:25 +0900

    ローカル実行環境整備

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,18 @@
-import {Hono} from "hono"
-import {handle} from "hono/aws-lambda"
+import { Hono } from "hono"
+import { serve } from "@hono/node-server";
+import { handle } from "hono/aws-lambda"
 
 const app = new Hono()
 
 app.get("/",  (c)=>{
   return c.text("Hello Hono!")
 })
+
+// ローカルサーバ用のコード
+if (process.env.NODE_ENV !== 'production') {
+  const port = 8787;
+  console.log(`Server running at http://localhost:${port}`)
+  serve({fetch: app.fetch, port})
+}
 
 export const handler=handle(app)
--- lintError.ts
+++
@@ -1,1 +0,0 @@
-const unusedVar = "this variable is not used"; // Lintエラーを発生させる
